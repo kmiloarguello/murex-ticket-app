6d43c49108a58d01ae848d2003c58abf2957cd8b
Camillo Arguello <camilo.arguello.rincon@gmail.com>
2021-11-12 20:56:18 +0100

    [CA] Routing for /ticket

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,15 @@ const routes: Routes = [
     ]
   },
   {
+    path:'ticket',
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./pages/ticket/ticket.module').then((m) => m.TicketModule)
+      }
+    ]
+  },
+  {
     path:'',
     children: [
       {
