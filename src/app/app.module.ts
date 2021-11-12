15194fbe45c947a5bd969497541787591d6fec33
Camillo Arguello <camilo.arguello.rincon@gmail.com>
2021-11-12 21:16:59 +0100

    [CA] Clean before ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TicketsComponent } from './pages/tickets/tickets.component';
-import { TicketComponent } from './pages/ticket/ticket.component';
-import { CardComponent } from './components/card/card.component';
-import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [AppComponent],
